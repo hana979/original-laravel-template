f7678dddc96b7fc03ac0e735ef872195d7d42d82
hana979 <tosiki0999@gmail.com>
2023-11-23 23:06:29 +0900

    feat: 共通例外処理ハンドラを設定

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -22,5 +25,36 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json(['message' => 'Model not found'], 404);
+        } elseif ($e instanceof NotFoundHttpException) {
+            return response()->json(['message' => 'Not found'], 404);
+        } elseif ($e instanceof AuthenticationException) {
+            return response()->json(['message' => 'Unauthenticated'], 401);
+        } elseif ($e instanceof HttpException) {
+            return response()->json(['message' => $e->getMessage()], $e->getStatusCode());
+        } elseif ($e instanceof ValidationException) {
+            return response()->json(
+                [
+                    'message' => 'Validation error',
+                    'errors' => $e->errors(),
+                ],
+                422
+            );
+        } else {
+            // return parent::render($request, $e);
+            logger(__METHOD__ . ': ' . print_r($e, true));
+            $result = [
+                'message' => 'Internal server error',
+            ];
+            if (config('app.debug')) {
+                $result['error'] = $e->getMessage();
+            }
+            return response()->json($result, 500);
+        }
     }
 }
